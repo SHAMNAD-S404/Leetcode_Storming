d6ffb667c72d2302095096e90be5e0db83bddbcf
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2024-11-26 09:43:55 +0530

    Time: 2 ms (38.07%), Space: 52.3 MB (42.05%) - LeetHub

---
+++ 3636-check-balanced-string/3636-check-balanced-string.ts
@@ -0,0 +1,19 @@
+function isBalanced(num: string): boolean {
+
+    const numArray : number[] = num.split("").map( no => Number(no) )
+    const size : number = numArray.length;
+    let evenSum : number = 0;
+    let oddSum : number = 0;
+    
+    for(let i=0; i<size; i++){
+        
+        if( i%2 === 1 ){
+            oddSum += numArray[i];
+        }else{
+            evenSum += numArray[i];
+        }
+    }
+
+    return oddSum === evenSum;
+    
+};

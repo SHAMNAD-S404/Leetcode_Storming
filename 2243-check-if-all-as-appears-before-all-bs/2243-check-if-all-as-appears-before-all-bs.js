4606073a4c06aa7a267fed4907153329de562439
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2024-12-02 23:04:53 +0530

    Time: 0 ms (100%), Space: 49.5 MB (21.37%) - LeetHub

---
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkString = function(s) {
+
+    let seenB = false; // Track if we have encountered a 'b'
+    
+    for (let char of s) {
+        if (char === 'b') {
+            seenB = true; // Start tracking 'b'
+        }
+        if (char === 'a' && seenB) {
+            return false; // 'a' after 'b' found
+        }
+    }
+    
+    return true; // No violations found
+    
+};

19ae229ea996c675f2a5a2910f724b002ccc10bd
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2024-12-08 23:48:07 +0530

    Time: 0 ms (100%), Space: 52.8 MB (50%) - LeetHub

---
+++ 0861-flipping-an-image/0861-flipping-an-image.ts
@@ -0,0 +1,8 @@
+function flipAndInvertImage(image: number[][]): number[][] {
+    
+      return image.map(row =>
+        row
+            .reverse() // Flip the row horizontally
+            .map(pixel => 1 - pixel) // Invert each pixel
+    );
+};

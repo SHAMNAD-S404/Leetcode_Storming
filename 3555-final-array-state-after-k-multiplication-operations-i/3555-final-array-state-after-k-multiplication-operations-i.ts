05d0c7a93f580b86db238a68cb118d5ff00fbe8b
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2024-12-10 22:35:53 +0530

    Time: 2 ms (50%), Space: 53.3 MB (31.71%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.ts
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.ts
@@ -3,8 +3,8 @@ function getFinalState(nums: number[], k: number, multiplier: number): number[] 
 
     for(let i=0;i<k;i++){
 
-        let min = Math.min(...nums)
-        let index = nums.indexOf(min)
+        let min = Math.min(...nums);
+        let index = nums.indexOf(min); //finding the first occurence of min value
         nums[index] = nums[index] * multiplier ;
     }
 

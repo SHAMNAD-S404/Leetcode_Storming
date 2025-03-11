fa17f411b0db90b571064ffda4845dea1f43e850
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-03-11 21:35:16 +0530

    Time: 25 ms (11.07%), Space: 61.4 MB (35.69%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.ts
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.ts
@@ -4,6 +4,7 @@ function findMissingAndRepeatedValues(grid: number[][]): number[] {
     const result : number [] = [];
     const seen  = new Set<number>();
 
+    //1. using set to track the duplicate number
     for(const num of arr){
         if( seen.has(num) ){
             result.push(num)
@@ -13,14 +14,14 @@ function findMissingAndRepeatedValues(grid: number[][]): number[] {
         }
     }
 
-
+    //2. finding the missed value from the array;
     for(let i = 1 ; i<= arr.length ;i++){
         if( !arr.includes(i) ){
             result.push(i);
             break;
         }
     }
-
+    //3. return result
     return result ;
     
 };

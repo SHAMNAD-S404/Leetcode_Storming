7ad71e184abb9d6c57a3d8d2a55b4aadb9c23732
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-04-07 12:12:54 +0530

    Time: 52 ms (12.91%), Space: 54 MB (11.88%) - LeetHub

--- 1970-sorting-the-sentence/1970-sorting-the-sentence.js
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.js
@@ -4,15 +4,15 @@
  */
 var sortSentence = function(s) {
 
-    const arr = s.split(" "); //converting to array
-    let result = [] ; 
+    const arr = s.split(" ")
+    let result = []
 
-    //sorting the array of string based on the last number in ascending order
-    const sort = arr.sort((a,b) => a.split("").pop() - b.split("").pop() );
+    const sort = arr.sort((a,b) => a.split("").pop() - b.split("").pop() )
 
-    for(let i = 0 ; i<sort.length ;i++){  
-        result.push( sort[i].slice(0,-1) ); //addding the string to array except that last integer
+    for(let i=0; i<sort.length; i++){
+        result.push(sort[i].slice(0,-1))
     }
 
-    return result.join(" ")   
+    return result.join(" ")
+    
 };

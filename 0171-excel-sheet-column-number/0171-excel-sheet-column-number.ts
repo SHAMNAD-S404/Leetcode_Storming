1249ff65d5b1a6361b33d5ef82be4dc799c9e25e
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-05-28 23:27:37 +0530

    Time: 1 ms (49.7%), Space: 58.7 MB (9.09%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.ts
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.ts
@@ -3,9 +3,9 @@ function titleToNumber(columnTitle: string): number {
     let result : number  = 0;
     
     for(let i =0; i<columnTitle.length; i++){
-
         const value = columnTitle.charCodeAt(i) - "A".charCodeAt(0) + 1;
         result = result * 26 + value;
     }
+    
     return result;
 };

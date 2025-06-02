ae991b54371a9af1ac4252254539c32535bcdf70
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-06-02 23:27:04 +0530

    Time: 50 ms (21.64%), Space: 55.5 MB (34.92%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.ts
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.ts
@@ -1,7 +1,6 @@
 type Fn = (n: number, i: number) => any
 
-function filter(arr: number[], fn: Fn): number[] {
-    
+function filter(arr: number[], fn: Fn): number[] { 
     return arr.filter( (num,index) => {
          return fn(num,index)
     });

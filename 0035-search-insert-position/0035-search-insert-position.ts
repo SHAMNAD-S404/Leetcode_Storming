e12cbd5dc6e617232565b6dff3064b67652aa3e7
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-06-05 23:43:04 +0530

    Time: 1 ms (16.39%), Space: 56.1 MB (20.65%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -0,0 +1,20 @@
+function searchInsert(nums: number[], target: number): number {
+
+    // edge case
+    if(nums.length === 0) return 0;
+    
+    for(let i=0; i<nums.length; i++){
+        // after find the target return index
+        if(nums[i] === target){
+            return i;
+        }
+        // if target is not found and array element is larger than target return the postion
+        if(nums[i] > target){
+            return i;
+        }
+    }
+    // edge case
+    // if target is higher than all the elements in the array then retun the length of the array as index
+    return nums.length;
+    
+};

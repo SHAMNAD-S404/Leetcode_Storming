7bb20165fa3399be14715442eee99201a588fdc0
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-06-25 15:34:38 +0530

    Time: 2 ms (75.02%), Space: 56.9 MB (47.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,21 +1,16 @@
 function twoSum(nums: number[], target: number): number[] {
 
-    //1. Initialize Map structure for storing difference with indices
-    const diffMap = new Map <number, number>();
+    const diffMap  = new Map<number,number>();
 
-    //2. loop through each array elements
-    for (let i=0; i<nums.length; i++){
+    for (let i=0; i<nums.length; i++) {
 
-        //3. find the difference
         let difference = target - nums[i];
 
-        //4. check for its exist then return indices
-        if( diffMap.has(difference) ){
-            return[i,diffMap.get(difference) ]
+        if(diffMap.has(difference) ){
+            return [ diffMap.get(difference), i ];
         }
 
-        //5. If its didn't exist add the difference and indices to the map
         diffMap.set(nums[i],i)
-
-    }    
+    }
+    
 };

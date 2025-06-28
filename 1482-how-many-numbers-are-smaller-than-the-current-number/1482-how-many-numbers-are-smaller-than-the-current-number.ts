9957939647866fca76af908997c67ecd713c87a6
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-06-28 20:59:01 +0530

    Time: 2 ms (96.92%), Space: 59 MB (25.38%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.ts
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.ts
@@ -1,13 +1,14 @@
 function smallerNumbersThanCurrent(nums: number[]): number[] {
-    
-    const sorted : number[] = [...nums].sort((a,b) => a-b );
-    const map  = new Map<number,number>();
 
-    for(let i=0; i<sorted.length; i++ ){
-        if(!map.has(sorted[i]) ){
-            map.set(sorted[i],i)
+    const sorted : number[] = [...nums].sort((a,b) => a-b );
+    const map = new Map<number,number> ();
+
+    for(let i=0; i<sorted.length; i++){
+        if( !map.has(sorted[i]) ){
+            map.set(sorted[i],i);
         }
     }
 
-    return nums.map((nums) => map.get(nums) );
+    return nums.map( (number) => map.get(number) );
+    
 };

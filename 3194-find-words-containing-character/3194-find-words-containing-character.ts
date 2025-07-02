1062a4e5b1f3710c0b3a531db916006467a7dad8
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-07-02 19:01:53 +0530

    Time: 1 ms (84.35%), Space: 58.4 MB (89.7%) - LeetHub

--- 3194-find-words-containing-character/3194-find-words-containing-character.ts
+++ 3194-find-words-containing-character/3194-find-words-containing-character.ts
@@ -1,6 +1,12 @@
 function findWordsContaining(words: string[], x: string): number[] {
 
-    return words.map((word , index) => word.includes(x) ? index : -1 ).
-            filter(index => index !== -1)
+    const result : number [] = [];
+
+    for(let i=0; i<words.length; i++){
+        if(words[i].includes(x)) {
+            result.push(i)
+        }
+    }
+    return result;
     
 };

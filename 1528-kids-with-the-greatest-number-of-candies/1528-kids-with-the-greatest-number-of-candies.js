7b002be9515e039f1d7d96f51a8cf0e63f50374a
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-07-06 09:53:22 +0530

    Time: 0 ms (100%), Space: 56 MB (13.26%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.js
@@ -6,16 +6,7 @@
 
 var kidsWithCandies = function(candies, extraCandies) {
 
-// we need a loop 2. var assign i + extra 3 . Math.max(var,...arr) 4. var === max 5. push to arr
-const result = [];
-for(const num of candies){
-
-    let luckyKid = num + extraCandies;
-    let max = Math.max(luckyKid,...candies);
-    luckyKid == max ? result.push(true) : result.push(false)
-
-}
-return result;
-
+const maxCandie = Math.max(...candies);
+return candies.map((candie) => (candie+extraCandies) >= maxCandie ? true : false);
 
 };

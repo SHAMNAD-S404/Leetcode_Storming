96861b0019d9072ecd0da4f84bfdb1030d277b5a
SHAMNAD-SKR <155313225+SHAMNAD-S404@users.noreply.github.com>
2025-07-12 17:35:45 +0530

    Time: 3 ms (41.1%), Space: 57.6 MB (86.3%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.ts
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.ts
@@ -1,28 +1,23 @@
 function maxFrequencyElements(nums: number[]): number {
 
     const map = new Map<number,number>();
+    let maxFreq : number = 0;
 
+    //step 1 : Loop through array and find max frequencies and its count
     for(const num of nums){
+        const count = (map.get(num) || 0) + 1;
+        map.set(num,count); //updating map
+        // updating the maxFreq to get the highest frequency
+        maxFreq = Math.max(maxFreq,count) 
+    }
 
-        if(!map.has(num)){
-            map.set(num,1)
-        }else{
-            map.set(num,map.get(num)+1) ;
+    //step 2 : loop through the Map and find which elements have same freq and count it.
+    let total : number = 0;
+    for( const value of map.values() ){
+        if(value === maxFreq){
+            total += value;
         }
     }
-    // step 2 : sort key values
-    const myMap = Array.from(map).sort((a,b) => b[1] - a[1]);
-
-    //step 3 : find max frequency
-    const maxFrequency : number = myMap[0][1];
-
-    // step 4 : loop through the array and find the total sum
-    let sum : number = 0;
-    for(const [num,freq] of myMap){
-        if(freq === maxFrequency){
-            sum += freq;
-        }
-    }
+    return total;
     
-    return sum
 };
